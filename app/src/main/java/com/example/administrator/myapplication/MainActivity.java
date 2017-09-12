6a8cbb2febea08bbf7dc596832339b1e3f817495
userliuguangxu <814624239@qq.com>
2017-09-12 11:48:32 +0800

    修改后2

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -10,6 +10,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        initDatao();
+    }
+
+    private void initDatao() {
     }
 
     private void init() {
